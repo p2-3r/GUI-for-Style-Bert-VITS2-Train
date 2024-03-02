71fe9ba78b2b4c6da31f90c7554588241021fdd2
p2-3r <159620178+p2-3r@users.noreply.github.com>
2024-03-03 04:15:56 +0900

    空白が含まれたパスで実行したときに実行できないバグを修正

--- run.bat
+++ run.bat
@@ -8,7 +8,7 @@ cd %OUTPUT%
 if exist "./venv/Scripts/activate" (
     call "./venv/Scripts/activate"
     echo [34mINFO[0m^|[32m%time:~0,2%:%time:~3,2%:%time:~6,2%[0m^|Running tool...
-    python %PYDIR%main.py %PYDIR%
+    python "%PYDIR%main.py"
 ) else (
     echo [31mERROR[0m: Could not start Style-Bert-VITS2 venv
     pause
