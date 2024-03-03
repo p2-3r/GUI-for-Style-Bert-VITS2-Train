6dc235d81f765a5ece03e0e170e8136cfb413388
p2-3r <garb_chips0x@icloud.com>
2024-03-03 22:09:07 +0900

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,2 +1,9 @@
+@echo off
+
+echo [34mINFO[0m^|[32m%time:~0,2%:%time:~3,2%:%time:~6,2%[0m^|Starting update...
+
 git pull
 git merge origin/dev
+
+echo echo [33mCOMPLETE[0m^|[32m%time:~0,2%:%time:~3,2%:%time:~6,2%[0m^|Update completed successfully!
+pause
