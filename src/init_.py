2272203708c9bec5eafca724276b2d9db581e135
p2-3r <garb_chips0x@icloud.com>
2024-03-03 21:39:13 +0900

    settings.jsonがないと生成するように変更

--- src/init_.py
+++ src/init_.py
@@ -28,7 +28,49 @@ class Jsontools():
         settings["style_path"] = path
         self.write(settings)
 
+    @classmethod
+    def create(self):
+        with open('settings.json', "w", encoding="utf-8") as f:
+            data = {
+                        "style_path": "",
+                        "slice_settings": {
+                            "open_path": "",
+                            "min_sec": 2.0,
+                            "max_sec": 12.0,
+                            "min_silence_dur_ms": 700
+                        },
+                        "transcribe_settings": {
+                            "device": "cuda",
+                            "lang": "ja",
+                            "whisper": "large-v3",
+                            "num": "bfloat16",
+                            "prompt": "こんにちは。元気、ですかー？ふふっ、私は……ちゃんと元気だよ！"
+                        },
+                        "Train_settings": {
+                            "jp_extra": True,
+                            "batch_size": 2,
+                            "epoch": 100,
+                            "step": 1000,
+                            "normalize": False,
+                            "delete_silent": False,
+                            "cant_read_file": "エラー出たらテキスト前処理が終わった時点で中断",
+                            "process": 6,
+                            "ver_data": 0,
+                            "tensorb": 200,
+                            "freeze_en": False,
+                            "freeze_ja": False,
+                            "freeze_zh": False,
+                            "freeze_style": False,
+                            "freeze_decoder": False
+                        }
+                    }
+
+            json.dump(data, f, indent=4, ensure_ascii=False)
+
 if __name__ == "__main__":
+
+    if not Path("settings.json").exists():
+        Jsontools.create()
 
     settings = Jsontools.read()
     style_path = Jsontools.read_stylepath()
