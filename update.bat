916f3b8244fd062f5392ee9113ef96f7498fcfc5
p2-3r <garb_chips0x@icloud.com>
2024-03-03 22:37:35 +0900

    Update update.bat

--- update.bat
+++ update.bat
@@ -3,7 +3,7 @@
 echo [34mINFO[0m^|[32m%time:~0,2%:%time:~3,2%:%time:~6,2%[0m^|Starting update...
 
 git fetch origin
-git reset --hard origin/dev
+git reset --hard origin/main
 
 echo [33mCOMPLETE[0m^|[32m%time:~0,2%:%time:~3,2%:%time:~6,2%[0m^|Update completed successfully!
 pause
